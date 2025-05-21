3db76abbef124f46d32390920028ca54a101e3ee
“Mukti” <muktijindal5@gmail.com>
2025-05-22 01:49:47 +0530

    changes in service card and component

--- src/app/components/cards/ServiceCard.jsx
+++ src/app/components/cards/ServiceCard.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 export const ServiceCard = ({ title, description, image }) => {
   return (
-    <div className="rounded-3xl border p-4 shadow-md bg-white w-full max-w-xs sm:max-w-sm flex-shrink-0">
+    <div className="rounded-3xl border p-4 shadow-md bg-white w-full max-w-xs sm:max-w-sm flex-shrink-0 border-[#1B1A1C14]">
 
 
-    <div className="rounded-3xl border p-4 shadow-md bg-white w-full max-w-xs sm:max-w-sm flex-shrink-0">
+    <div className="rounded-3xl border p-4 shadow-md bg-white w-full max-w-xs sm:max-w-sm flex-shrink-0 border-[#1B1A1C14]">
       <img
         src={image}
         alt={title}
         className="rounded-xl w-full h-[150px] object-cover"
       />
-      <div className="border mt-4 rounded-2xl p-4">
+      <div className="border mt-4 rounded-2xl p-4 border-[#1B1A1C14]"> 
         <p className="bg-black text-white inline-block px-4 py-2 rounded-xl mb-4">
           {title}
         </p>
