8f5c07e115b4a44ef27bbc312f1632ac530cbdc3
“Mukti” <muktijindal5@gmail.com>
2025-05-24 17:26:23 +0530

    fix the service slider

--- src/app/components/Landing/Services.jsx
+++ src/app/components/Landing/Services.jsx
@@ -87,7 +87,7 @@ export const Services = () => {
             breakpoints={{
               640: { slidesPerView: 1.5 },
               768: { slidesPerView: 2 },
-              1024: { slidesPerView: 3 },
+              1024: { slidesPerView: 4 },
             }}
           >
             {services.map((service, index) => (
